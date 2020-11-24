d7104f084eef1ad3f8d69770a6a34792aa93512e
Kevin <kjayh@unal.edu.co>
2020-11-24 15:55:43 -0500

    Busqueda 2.0
    actualizacion de las funciones de eleccion de clases y atributos

--- src/main/java/org/example/MenuBusqueda.java
+++ src/main/java/org/example/MenuBusqueda.java
@@ -2,6 +2,7 @@ package org.example;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -20,6 +21,80 @@ public class MenuBusqueda {
 
     @FXML
     public TextField caracterABuscar;
+
+    @FXML
+    public CheckBox Area;
+
+    @FXML
+    public CheckBox Empleado;
+
+    @FXML
+    public CheckBox Cliente;
+
+    @FXML
+    public CheckBox Atributo1;
+
+    @FXML
+    public CheckBox Atributo2;
+
+    @FXML
+    public CheckBox Atributo3;
+
+    @FXML
+    private void SeleccionArea(ActionEvent event) throws IOException{
+        if(Area.isSelected()){
+            Empleado.setSelected(false);
+            Cliente.setSelected(false);
+            Atributo1.setText("Nombre");
+            Atributo2.setText("Ingresos");
+            Atributo3.setText("Sede");
+        }
+    }
+
+    @FXML
+    private void SeleccionEmpleado(ActionEvent event) throws IOException{
+        if(Empleado.isSelected()){
+            Area.setSelected(false);
+            Cliente.setSelected(false);
+            Atributo1.setText("Cedula");
+            Atributo2.setText("Edad");
+            Atributo3.setText("Nombre");
+        }
+    }
+
+    @FXML
+    private void SeleccionCliente(ActionEvent event) throws IOException{
+        if(Cliente.isSelected()){
+            Area.setSelected(false);
+            Empleado.setSelected(false);
+            Atributo1.setText("Cedula");
+            Atributo2.setText("Dinero");
+            Atributo3.setText("Nombre");
+        }
+    }
+    @FXML
+    private void SeleccionarAtributo1(ActionEvent event)throws IOException{
+        if(Atributo1.isSelected()){
+            Atributo2.setSelected(false);
+            Atributo3.setSelected(false);
+        }
+    }
+
+    @FXML
+    private void SeleccionarAtributo2(ActionEvent event)throws IOException{
+        if(Atributo2.isSelected()){
+            Atributo1.setSelected(false);
+            Atributo3.setSelected(false);
+        }
+    }
+
+    @FXML
+    private void SeleccionarAtributo3(ActionEvent event) throws IOException{
+        if(Atributo3.isSelected()){
+            Atributo1.setSelected(false);
+            Atributo2.setSelected(false);
+        }
+    }
 
     @FXML
     private void Buscar(ActionEvent salir)throws IOException{
