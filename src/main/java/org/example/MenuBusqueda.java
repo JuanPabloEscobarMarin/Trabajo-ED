77c892b4a34d62f8830acffca9747018be77ec95
Miguel Olave <molave@unal.edu.co>
2020-11-24 15:03:23 -0500

    Proceso interfaz busqueda

--- src/main/java/org/example/MenuBusqueda.java
+++ src/main/java/org/example/MenuBusqueda.java
@@ -1,4 +1,39 @@
 package org.example;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class MenuBusqueda {
+
+    @FXML
+    public Label ErrorcaracterABuscar;
+
+    @FXML
+    public Label ErroropcionesClase;
+
+    @FXML
+    public Label ErroropcionesAtributo;
+
+    @FXML
+    public TextField caracterABuscar;
+
+    @FXML
+    private void Buscar(ActionEvent salir)throws IOException{
+
+
+        //Proceso de busqueda
+
+        App.setRoot("MenuUsuario");
+    }
+
+    @FXML
+    private void Salir(ActionEvent salir)throws IOException {
+        App.setRoot("MenuUsuario");
+    }
+
+
 }
